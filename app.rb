eab93fbe1c82fefa4f15751c08c41dd99b40c257
Linda Luu and Megan Olson <megan@>
2017-09-27 09:12:11 -0700

    add route to display recipe details

--- app.rb
+++ app.rb
@@ -12,7 +12,16 @@ end
 post '/recipe' do
   @recipes = Recipe.all
   recipe_title = params["recipe_title"]
-  @recipe = Recipe.new({title: recipe_title})
+  ingredients = params["ingredients"]
+  instructions = params["instructions"]
+  @recipe = Recipe.new({title: recipe_title, ingredients: ingredients, instructions: instructions})
   @recipe.save
   erb(:index)
+end
+
+get '/recipes/:id' do
+  @recipe = Recipe.find(params[:id])
+  @ingredients = @recipe.ingredients
+  @instructions = @recipe.instructions
+  erb(:recipes)
 end
